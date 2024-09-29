1c13aa2e4637ee5bc7e0e9df13e7b33537bf8958
carlos-zh <1216276630@qq.com>
2024-09-29 15:51:14 +0800

    第一次上传：该项目用于测试各种Spring原理相关问题，以及学习

---
+++ src/main/java/com/zgl/MySpringTestApplication.java
@@ -0,0 +1,13 @@
+package com.zgl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zgl/MySpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zgl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
